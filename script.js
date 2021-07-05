960cbb14c7c88c4bf674e9fef7d76bd6f02edcfa
ahmetbozaci <ahmt9417@gmail.com>
2021-07-05 19:58:45 +0300

    Fix eslint and stylelint errors

--- script.js
+++ script.js
@@ -1,21 +1,22 @@
-const bookArray = []
+/* eslint-disable no-unused-vars */
+const bookArray = [];
 
-function Book (title, author) {
-    this.title = title;
-    this.author = author;
+function Book(title, author) {
+  this.title = title;
+  this.author = author;
 }
 function addBook() {
-  let title = document.getElementById("title").value
-  let author = document.getElementById("author").value
+  const title = document.getElementById('title').value;
+  const author = document.getElementById('author').value;
   const newBook = new Book(title, author);
-  bookArray.push(newBook)
+  bookArray.push(newBook);
 }
 
 function displayBooks() {
-    let book = ""
-    for (let i = 0; i < bookArray.length; i++){ 
-       book = `${bookArray[i].title} ${bookArray[i].author} <br>`
+  let book = '';
+  for (let i = 0; i < bookArray.length; i += 1) {
+    book = `${i + 1}. ${bookArray[i].title} - ${bookArray[i].author} <br>`;
   }
-  document.getElementById("bookList").innerHTML += book
+  document.getElementById('bookList').innerHTML += book;
 }
-
+/* eslint-enable no-unused-vars */
