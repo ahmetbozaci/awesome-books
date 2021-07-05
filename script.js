13e3d6d736a009dfae1a844c6afd14f5de27583e
ahmetbozaci <ahmt9417@gmail.com>
2021-07-05 16:18:10 +0300

    Create add book function

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
+const bookArray = []
+
 function Books (title, author) {
     this.title = title;
     this.author = author;
 }
+Books.prototype.addBook = function () {
+  let title = document.getElementById("title").value
+  let author = document.getElementById("author").value
+  const newBook = new Books(title, author);
+  bookArray.push(newBook)
+}
 
-const bookInfo = new Books("1984", "Author Name");
-
-
-document.getElementById("boo").innerHtml = bookInfo.title + bookInfo.author;
+// document.getElementById("boo").innerHTML = bookInfo.title + bookInfo.author;
