ba6c5e2b2bed0b57d791153a7126861e82c4a2f6
elyor doniyorov <doniyorovelyor@gmail.com>
2021-07-05 18:45:12 +0500

    modify index and script files

--- script.js
+++ script.js
@@ -4,11 +4,17 @@ function Books (title, author) {
     this.title = title;
     this.author = author;
 }
-Books.prototype.addBook = function () {
+function addBook () {
   let title = document.getElementById("title").value
   let author = document.getElementById("author").value
   const newBook = new Books(title, author);
   bookArray.push(newBook)
 }
 
-// document.getElementById("boo").innerHTML = bookInfo.title + bookInfo.author;
+function displayBooks () {
+    for (let i = 0; i < bookArray.length; i++) {
+        console.log(bookArray[i].title);
+    }
+}
+
+document.getElementById("boo").innerHTML = bookInfo.title + bookInfo.author;
