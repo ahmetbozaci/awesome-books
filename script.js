5b1173fb6580ba28430d8d3ca185756ea59b4672
ahmetbozaci <ahmt9417@gmail.com>
2021-07-05 18:41:06 +0300

    Update display book function

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ function Books (title, author) {
     this.title = title;
     this.author = author;
 }
-function addBook () {
+function addBook() {
   let title = document.getElementById("title").value
   let author = document.getElementById("author").value
   const newBook = new Books(title, author);
@@ -12,14 +12,10 @@ function addBook () {
 }
 
 function displayBooks() {
-    
+    let book = ""
     for (let i = 0; i < bookArray.length; i++){ 
-      let item = document.createElement("li"); 
-      let title = document.createTextNode("titletest")
-      //let title = bookArray[i].title;
-      //let author = bookArray[i].author;
-      item.appendChild(title)
-      document.getElementById("bookList").appendChild(item);
+       book += `${bookArray[i].title} ${bookArray[i].author} <br>`
   }
+  document.getElementById("bookList").innerHTML = book 
 }
 
