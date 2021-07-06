aa2867526103d843f4f91f0a0931cf4b8313a3ee
ahmetbozaci <ahmt9417@gmail.com>
2021-07-06 12:53:08 +0300

    Add local storage

--- script.js
+++ script.js
@@ -1,25 +1,26 @@
 /* eslint-disable no-unused-vars */
-const bookArray = [];
+let bookArray = localStorage.getItem('books') ? JSON.parse(localStorage.getItem('books')) : [];
+localStorage.setItem('books', JSON.stringify(bookArray));
+const library = JSON.parse(localStorage.getItem('books'));
 
 function Book(title, author) {
   this.title = title;
   this.author = author;
 }
+
+function createList(title,author){
+  const li = document.createElement('li')
+  li.textContent = `${title} - by ${author}` 
+  const ul = document.getElementById("bookList")
+  ul.appendChild(li);
+}
+
 function addBook() {
-  const title = document.getElementById('title').value;
-  const author = document.getElementById('author').value;
+  let title = document.getElementById('title').value;
+  let author = document.getElementById('author').value;
   const newBook = new Book(title, author);
   bookArray.push(newBook);
+  localStorage.setItem('books', JSON.stringify(bookArray))
+  createList(title,author);
 }
-
-function displayBooks() {
-  let book = '';
-  for (let i = 0; i < bookArray.length; i += 1) {
-    book = `<li id="${i + 1}"> ${bookArray[i].title} - ${bookArray[i].author} <button type='button' onclick="removeBook()">Remove Book</button><li><hr>  `;
-  }
-  document.getElementById('bookList').innerHTML += book;
-}
-
-function removeBook() {
-}
-/* eslint-enable no-unused-vars */
+library.forEach(book => {createList(book.title,book.author)})
