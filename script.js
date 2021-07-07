8fb438666d456bdaf75a654c0590d6047b462022
ahmetbozaci <ahmt9417@gmail.com>
2021-07-07 12:23:20 +0300

    Fix linter errors

--- script.js
+++ script.js
@@ -1,45 +1,42 @@
 /* eslint-disable no-unused-vars */
-let bookArray = localStorage.getItem('books') ? JSON.parse(localStorage.getItem('books')) : [];
+const bookArray = localStorage.getItem('books') ? JSON.parse(localStorage.getItem('books')) : [];
 localStorage.setItem('books', JSON.stringify(bookArray));
 const library = JSON.parse(localStorage.getItem('books'));
 
 function Book(title, author) {
-this.title = title;
-this.author = author;
+  this.title = title;
+  this.author = author;
 }
 
-let i = 1
-function removeBook(e) {
-let takeItem = document.querySelectorAll('.rmItem')
-let bookIndex = Array.prototype.indexOf.call(takeItem, e.target)
-bookArray.splice(bookIndex, 1);
-console.log(bookArray)
-localStorage.setItem('books', JSON.stringify(bookArray))
-console.log(localStorage)
-const updatedLibrary = JSON.parse(localStorage.getItem('books'));
-document.getElementById('bookList').innerHTML = "";
-updatedLibrary.forEach(item => {createList(item.title, item.author)})
-}
 function createList(title, author) {
-const li = document.createElement('li');
-li.textContent = `${title} - by ${author}`;
-let button = document.createElement('button');
-button.classList.add('rmItem')
-button.innerHTML = "Remove";
-button.addEventListener('click', removeBook)
-li.appendChild(button);
-const ul = document.getElementById("bookList")
-ul.appendChild(li);
+  const li = document.createElement('li');
+  li.textContent = `${title} - by ${author}`;
+  const button = document.createElement('button');
+  button.setAttribute('class', 'rmItem');
+  button.innerHTML = 'Remove';
+  button.addEventListener('click', removeBook);
+  li.appendChild(button);
+  const ul = document.getElementById('bookList');
+  ul.appendChild(li);
+}
+
+function removeBook(e) {
+  const takeItem = document.querySelectorAll('.rmItem');
+  const bookIndex = bookArray.indexOf.call(takeItem, e.target);
+  bookArray.splice(bookIndex, 1);
+  localStorage.setItem('books', JSON.stringify(bookArray));
+  const updatedLibrary = JSON.parse(localStorage.getItem('books'));
+  document.getElementById('bookList').innerHTML = '';
+  updatedLibrary.forEach((item) => { createList(item.title, item.author); });
 }
 
 function addBook() {
-let title = document.getElementById('title').value;
-let author = document.getElementById('author').value;
-const newBook = new Book(title, author);
-bookArray.push(newBook);
-localStorage.setItem('books', JSON.stringify(bookArray))
-createList(title,author);
+  const title = document.getElementById('title').value;
+  const author = document.getElementById('author').value;
+  const newBook = new Book(title, author);
+  bookArray.push(newBook);
+  localStorage.setItem('books', JSON.stringify(bookArray));
+  createList(title, author);
 }
 
-
-library.forEach(item => {createList(item.title, item.author)})
+library.forEach((item) => { createList(item.title, item.author); });
